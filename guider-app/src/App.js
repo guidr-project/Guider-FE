9e3534dfddd42d8b64b64eccdfdbd71a270a2f4b
MadisonViaLux <madison.s.mckown@gmail.com>
2019-09-23 15:22:49 -0700

    setting up a singup and routes

--- guider-app/src/App.js
+++ guider-app/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route, Redirect} from "react-router-dom";
+
+import SignUp from './components/loginInfo/SignUp'
+import { Profile } from './components/ProfileCards/Profile'
+import Links from './components/loginInfo/Links'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Route exact path='/' component={Links} />
+
+        <Route exact path='/signup' component={SignUp} />
+
+        <Route exact path='/profile' component={Profile} />
+
         Hello There
       </header>
     </div>
--- guider-app/src/components/loginInfo/Links.js
+++ guider-app/src/components/loginInfo/Links.js
@@ -1,1 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Links() {
+  return (
+    <div >
+      <Link className="navlink" to='/signup'>Sign Up</Link>
+      <Link className="navlink" to='/login'>Login</Link>
+    </div>
+  );
+}
+
+export default Links;
