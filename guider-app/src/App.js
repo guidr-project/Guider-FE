c57d35663ccfa6c102d31b5c691adf37bd609c63
Lamberto <asghedom.lamberto@gmail.com>
2019-09-23 14:45:38 -0700

    username and password fields

--- guider-app/src/App.js
+++ guider-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from './components/loginInfo/Login'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hello There
+        <Login />
       </header>
     </div>
   );
--- guider-app/src/components/loginInfo/Login.js
+++ guider-app/src/components/loginInfo/Login.js
@@ -1,1 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Formik, Form, Field, withFormik } from 'formik'
+// import * as Yup from 'Yup'
+import axios from 'axios'
+
+const Login = () => {
+    return (
+        <div>
+            <Form>
+                <label>
+                    Username: 
+                    <Field 
+                        type="text" 
+                        name="username" 
+                    />
+                </label>
+                <label>
+                    Password: 
+                    <Field 
+                        type="password" 
+                        name="password" 
+                    />
+                </label>
+                <button>Log In</button>
+            </Form>
+        </div>
+    )
+}
+
+const FormikLoginForm = withFormik({
+    mapPropsToValues({username, password}) {
+        return {
+            username: username || "",
+            password: password || ""
+        }
+    },
+    handleSubmit(values) {
+        console.log(values)
+    }
+})(Login)
+
+export default FormikLoginForm
