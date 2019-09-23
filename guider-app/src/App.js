18356d863fe84c37a3219a4119c393b37833810f
adkhiker <poirier.greg@gmail.com>
2019-09-23 18:33:17 -0400

    Basic tab/nav and routing

--- guider-app/src/App.js
+++ guider-app/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-//import Header from "./ProfileCards/Header.js";
+import Header from "./components/ProfileCards/Header.js";
+import TabNav from "./components/ProfileCards/TabNav.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello There
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <TabNav />
+      </Router>
     </div>
   );
 }
