8dbb8852fd6f8bcf88725e711054ef628040492b
Lamberto <asghedom.lamberto@gmail.com>
2019-09-23 14:53:36 -0700

    validation schema for username and password

--- guider-app/src/components/loginInfo/Login.js
+++ guider-app/src/components/loginInfo/Login.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { Formik, Form, Field, withFormik } from 'formik'
-// import * as Yup from 'Yup'
+import * as Yup from 'yup'
 import axios from 'axios'
 
-const Login = () => {
+const Login = ({ values }) => {
     return (
         <div>
             <Form>
@@ -12,6 +12,7 @@ const Login = () => {
                     <Field 
                         type="text" 
                         name="username" 
+                        placeholder="Username"
                     />
                 </label>
                 <label>
@@ -19,6 +20,7 @@ const Login = () => {
                     <Field 
                         type="password" 
                         name="password" 
+                        placeholder="password" 
                     />
                 </label>
                 <button>Log In</button>
@@ -34,6 +36,10 @@ const FormikLoginForm = withFormik({
             password: password || ""
         }
     },
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required('Username is required'),
+        password: Yup.string().required('Password is required')
+    }),
     handleSubmit(values) {
         console.log(values)
     }
