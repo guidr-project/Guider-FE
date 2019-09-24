fa5abbbe6d9eed2c9bfb983897dbc6247dcc7f82
Lamberto <asghedom.lamberto@gmail.com>
2019-09-24 15:48:03 -0700

    context boilerplate

---
+++ guider-app/src/context/GuidrContext.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect, createContext } from 'react'
+import axios from axios
+
+export const GuidrContext = createContext()
+
+export const GuidrProvider = props => {
+    const [state, setState] = useState([])
+
+    useEffect(() => {
+        axios.get() // <---------- API 
+            .then(res => console.log(res))
+            .catch(err => console.log(err.response))
+    }, [])
+
+    return (
+        <GuidrContext.Provider value={[state, setState]}>
+            {props.children}
+        </GuidrContext.Provider>
+    )
+}
