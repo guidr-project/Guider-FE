e7624839420b06a22e11a3f3c28a374d404c3c2a
bbaney <48811486+bbaney@users.noreply.github.com>
2019-09-24 17:07:13 -0500

    finished some styling on the usercard stuff, gonna add some spacing between them later

--- guider-app/src/components/ProfileCards/TripCard.js
+++ guider-app/src/components/ProfileCards/TripCard.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { withFormik, Form, Field, validateYupSchema, setNestedObjectValues} from "formik";
+import { withFormik, Form, Field, validateYupSchema} from "formik";
 import * as Yup from "yup";
 import Switch from "./Switch";
 import { TextArea } from "semantic-ui-react";
@@ -13,9 +13,7 @@ const TripCard = ({values, errors, touched, status}) => {
             setTrips([...trips, status])
         }
     },[status]);
-
-
-    //tripname, boolean, descr, date/time
+    
     return (
         <div className="trip-container">
             <img className="mountains" img src="https://images.unsplash.com/photo-1516655855035-d5215bcb5604?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
@@ -27,19 +25,19 @@ const TripCard = ({values, errors, touched, status}) => {
                         <label htmlFor="tripName"><h2>Trip Name</h2></label>
                             <Field className="trip-name" type="text" name="tripName" placeholder="Name of Trip"/>
                             {touched.tripName && errors.tripName && (
-                                <p className="error">{errors.tripName}</p>
+                                <p className="errors">{errors.tripName}</p>
                             )}
 
                         <label htmlFor="description"><h2>Give a description of your trip!</h2></label>
                             <TextArea className="desc" type="note" name="description" placeholder="Description of Trip"/>
                             {touched.description && errors.description && (
-                                <p className="error">{errors.description}</p>
+                                <p className="errors">{errors.description}</p>
                             )}
 
                         <label htmlFor="date"><h2>Dates?</h2></label>
                             <Field className="date-name" type="date" name="date" placeholder="Date of Trip"/>
                             {touched.date && errors.date && (
-                                <p className="error">{errors.description}</p>
+                                <p className="errors">{errors.description}</p>
                             )}
                         
                         <Switch
@@ -62,7 +60,7 @@ const FormikTripCard = withFormik({
             description: description || "",
         }
     },
-    validationSchema: Yup.object().shape({
+    validateYupSchema: Yup.object().shape({
         tripName: Yup.string().required("You must enter a trip name"),
         description: Yup.string().required("You must enter a trip description"),
     }),
