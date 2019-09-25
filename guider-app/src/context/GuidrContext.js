d96dc81ba172b6f4907fb9ebeaa15914b1fd60c5
MadisonViaLux <madison.s.mckown@gmail.com>
2019-09-25 15:58:20 -0700

    merge 12

--- guider-app/src/context/GuidrContext.js
+++ guider-app/src/context/GuidrContext.js
@@ -1,20 +1,25 @@
-import React, {useState, useEffect, createContext } from 'react'
-import axios from axios
+// import React, {useState, useEffect, createContext } from 'react'
+// import axios from axios
 
-export const GuidrContext = createContext()
+// export const GuidrContext = createContext()
 
-export const GuidrProvider = props => {
-    const [state, setState] = useState([])
+// export const GuidrProvider = props => {
+//     const [state, setState] = useState([])
 
-    useEffect(() => {
-        axios.get() // <---------- API 
-            .then(res => console.log(res))
-            .catch(err => console.log(err.response))
-    }, [])
+//     useEffect(() => {
+//         axios.get() // <---------- API 
+//             .then(res => console.log(res))
+//             .catch(err => console.log(err.response))
+//     }, [])
 
-    return (
-        <GuidrContext.Provider value={[state, setState]}>
-            {props.children}
-        </GuidrContext.Provider>
-    )
-}
+//     return (
+//         <GuidrContext.Provider value={[state, setState]}>
+//             {props.children}
+//         </GuidrContext.Provider>
+//     )
+// }
+
+
+import { createContext } from 'react';
+
+export const JourneyContext = createContext();
