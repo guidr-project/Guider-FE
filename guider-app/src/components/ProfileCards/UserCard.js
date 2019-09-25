5e13216ab6e04583b9705b0bb555574b7ec56b07
bbaney <48811486+bbaney@users.noreply.github.com>
2019-09-25 17:52:13 -0500

    changed styling on cards on Users.JS

--- guider-app/src/components/ProfileCards/UserCard.js
+++ guider-app/src/components/ProfileCards/UserCard.js
@@ -1,30 +1,23 @@
 import React from "react";
-import "semantic-ui-css/semantic.min.css";
+
 
 function UserCard(props) {
     console.log(props)
     return (
-        <div>
-            
-            <div className="ui grid">
-                <div className="row">
-                    <div className="ui card four column wide">
-                    <div className="content" key={props.id}>
-                        <div className="header">Trip: {props.title}</div>
-                        <div className="meta">Start Date: {props.startDate}</div>
-                        <div className="meta">End Date: {props.endDate}</div>
-                        <div className="description">
-                        <p>
-                            {props.description}
-                        </p>
-                        </div>
-                        <div className="meta">{props.type}</div>
-                    </div>
-                    <div className="extra content">
-                        <button className="ui blue button">Edit</button>
-                        <button className="ui red button">Delete</button>
-                    </div>
-                    </div>
+        <div className="user-card">
+            <div className="basic">
+                <div className="card-info" key={props.id}>
+                    <h2>Trip: <span>{props.title}</span></h2>
+                    <p>Start Date: <span>{props.startDate}</span></p>
+                    <p>End Date: <span>{props.endDate}</span></p>
+                    <p><span>{props.description}</span></p>
+                    <p><span>{props.type}</span></p>
+                    <p>How many hours? <span>{props.hours}</span></p>
+                    <p>How many days?: <span>{props.days}</span></p>
+                </div>
+                <div className="extra content">
+                    <button className="ui blue button">Edit</button>
+                    <button className="ui red button">Delete</button>
                 </div>
             </div>
         </div>
