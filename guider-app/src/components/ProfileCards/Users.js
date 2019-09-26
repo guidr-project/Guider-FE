daf076a86e1d627ebb260f99201fed5e367f4d2b
MadisonViaLux <madison.s.mckown@gmail.com>
2019-09-26 11:12:46 -0700

    styling

--- guider-app/src/components/ProfileCards/Users.js
+++ guider-app/src/components/ProfileCards/Users.js
@@ -16,7 +16,7 @@ export const Users = props => {
         <div className="card-container">
             <img className="forest" src="https://images.unsplash.com/photo-1461397821064-32d6b3c91b9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
             alt="woods"/>
-            <div >
+            <div className='testingDiv' >
 
                 {journeys.map((user, index) => {
                     // console.log(user)
--- guider-app/src/components/ProfileCards/YourTrips.js
+++ guider-app/src/components/ProfileCards/YourTrips.js
@@ -16,7 +16,7 @@ export const YourTrips = props => {
         <div className="card-container">
             <img className="forest" src="https://images.unsplash.com/photo-1461397821064-32d6b3c91b9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
             alt="woods"/>
-            <div >
+            <div className='testingDiv'>
 
                 {journeys.map(user => { if(user.user_id == localStorage.getItem('id')){
                         return (
